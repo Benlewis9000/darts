e464502dd50ea70d95c6d80037e60f28e2742790
benlewis9000 <benjaminlewis9000@gmail.com>
2024-08-14 21:37:08 +0100

    Move existing files into new ClientApp dir

--- src/app/model/round.ts
+++
@@ -1,7 +0,0 @@
-import { DartBoardValue } from "./dart-board";
-
-export type RoundThrow = 1 | 2 | 3;
-
-export type Round = { 
-    [K in RoundThrow]?: DartBoardValue;
-} 
--- src/app/store/rounds/rounds.actions.ts
+++
@@ -1,7 +0,0 @@
-import { createAction, props } from "@ngrx/store";
-import { Round, RoundThrow } from "../../model/round";
-import { DartBoardValue } from "../../model/dart-board";
-
-export const setCurrentRoundNumber = createAction('[Rounds] Current round number set', props<{currentRoundNumber: number}>());
-export const setCurrentThrow = createAction('[Rounds] Current throw set', props<{currentThrow: RoundThrow}>());
-export const setValue = createAction('[Rounds] Value for current throw set', props<{value: DartBoardValue | undefined}>());
